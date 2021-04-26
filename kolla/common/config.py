60a4a01bb92be766288494e1f8e883472f7a059b
Ole Kleinschmidt <ok@datenreisende.org>
2021-04-26 11:19:35 +0200

    cherry-pick adjutant from victoria

--- kolla/common/config.py
+++ kolla/common/config.py
@@ -283,6 +283,10 @@ SOURCES = {
         'type': 'url',
         'location': ('$tarballs_base/openstack/requirements/'
                      'requirements-${openstack_branch}.tar.gz')},
+    'adjutant-base': {
+        'type': 'url',
+        'location': ('$tarballs_base/openstack/adjutant/'
+                     'adjutant-${openstack_branch}.tar.gz')},
     'aodh-base': {
         'type': 'url',
         'location': ('$tarballs_base/openstack/aodh/'
@@ -1042,6 +1046,10 @@ USERS = {
     'masakari-user': {
         'uid': 42485,
         'gid': 42485,
+    },
+    'adjutant-user': {
+        'uid': 42486,
+        'gid': 42486,
     }
 }
 
